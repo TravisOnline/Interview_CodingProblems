c07017442760e7704cba5383e7a4a814312f8844
Travis <82448667+TravisOnline@users.noreply.github.com>
2022-03-03 21:29:16 +0000

    Update CodingProblem_interview.py
    Add runtime and lines processed

--- company1/CodingProblem_interview.py
+++ company1/CodingProblem_interview.py
@@ -1,11 +1,15 @@
-#Task: from a supplied CSV, create a new file for individual store numbers
-#In the each store file, list comma delimited SKUs and price with a 5% discount
+# Task: from a supplied CSV, create a new file for individual store numbers
+# In the each store file, list comma delimited SKUs and price with a 5% discount
+# Print the run time and lines processed to the console
 
 import csv
+import timeit
 
 file = 'in.csv'
 
 discount = .05
+
+lines_processed = 1
 
 
 def calc_price(old_price: float):
@@ -13,6 +17,8 @@ def calc_price(old_price: float):
     #Return float as a string to limit to 2 decimal places
     return "{:.2f}".format(old_price - this_discount)
 
+
+start = timeit.default_timer()
 
 with open(file) as f:
     dictReader = csv.DictReader(f)
@@ -30,5 +36,9 @@ with open(file) as f:
 
         rowToWrite.clear()
         processedStoreNo = row['Store']
+        lines_processed += 1
     newFile.close()
 f.close()
+
+stop = timeit.default_timer()
+print('Run Time: ', stop - start, ' Lines processed: ', lines_processed)
